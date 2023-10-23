bdb8ec321817ad4f01cf6762d0e84d6c23436b73
chryschi <121822197+chryschi@users.noreply.github.com>
2023-10-23 22:16:29 +0200

    Add Book constructor for storing book infos

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const myLibrary = [book1, book2];
 
-function Book() {}
+function Book(title, author, numberOfPages, wasRead) {
+  (this.title = title),
+    (this.author = author),
+    (this.pages = pages),
+    (this.read = read),
+    (this.info = function () {
+      return `${title} by ${author}, ${numberOfPages} pages, ` + wasRead
+        ? "not read yet"
+        : "read already";
+    });
+}
 
 function addBookToLibrary() {}
 
