2f43fe0bd09774055377d9bc05c4223f807e0695
chryschi <121822197+chryschi@users.noreply.github.com>
2023-10-23 22:31:28 +0200

    Add missing semicolons in Book constructor

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const myLibrary = [book1, book2];
 
 function Book(title, author, numberOfPages, wasRead) {
-  (this.title = title),
-    (this.author = author),
-    (this.numberOfPages = numberOfPages),
-    (this.wasRead = wasRead),
-    (this.info = function () {
-      return `${title} by ${author}, ${numberOfPages} pages, ` + wasRead
-        ? "not read yet"
-        : "read already";
-    });
+  this.title = title;
+  this.author = author;
+  this.numberOfPages = numberOfPages;
+  this.wasRead = wasRead;
+  this.info = function () {
+    return `${title} by ${author}, ${numberOfPages} pages, ` + wasRead
+      ? "not read yet"
+      : "read already";
+  };
 }
 
 function addBookToLibrary() {}
