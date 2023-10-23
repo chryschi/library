903c02490198c8a3f171947873e78e1cc48c8b28
chryschi <121822197+chryschi@users.noreply.github.com>
2023-10-23 22:27:01 +0200

    Remove errors from Book constructor

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const myLibrary = [book1, book2];
 function Book(title, author, numberOfPages, wasRead) {
   (this.title = title),
     (this.author = author),
-    (this.pages = pages),
-    (this.read = read),
+    (this.numberOfPages = numberOfPages),
+    (this.wasRead = wasRead),
     (this.info = function () {
       return `${title} by ${author}, ${numberOfPages} pages, ` + wasRead
         ? "not read yet"
