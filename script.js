87ab4d74755c6aa4b2ffc7b439b0619bc10cf9bf
chryschi <121822197+chryschi@users.noreply.github.com>
2023-10-23 22:03:46 +0200

    Add basic functions for library and a button

---
+++ script.js
@@ -0,0 +1,10 @@
+const myLibrary = [book1, book2];
+
+function Book() {}
+
+function addBookToLibrary() {}
+
+function showBooks(books) {}
+
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", addBookToLibrary);
